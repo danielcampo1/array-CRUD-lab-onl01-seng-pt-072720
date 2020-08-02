45cd90b255336eabf78a952c367ff03122221661
daniel campo <campo_daniel94@yahoo.com>
2020-08-02 15:29:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_dog = ["Meela", "Luna", "Thor", "Prince"]
-  my_dog.push("element")
+  my_dog.push("arrays")
 end
 
 def add_element_to_start_of_array(array, element)
